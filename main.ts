d9cfdd2ba0bdaec2de0abb482d709be6c1f7ef9a
jaime <jaimemen897@gmail.com>
2025-06-21 16:17:27 +0200

    Weather mcp

---
+++ main.ts
@@ -0,0 +1,27 @@
+﻿import {McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
+import {z} from "zod";
+import {StdioServerTransport} from "@modelcontextprotocol/sdk/server/stdio";
+
+const server = new McpServer({
+    name: 'Demo',
+    version: '0.0.1',
+})
+
+server.tool('fetch-weather', 'Tool to fetch the weather',
+    {
+        city: z.string().describe('City name'),
+    },
+    async ({city}) => {
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text: `El clima de ${city} es soleado`
+                }
+            ]
+        }
+    }
+)
+
+const transport = new StdioServerTransport()
+await server.connect(transport)
